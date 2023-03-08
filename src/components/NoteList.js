ad99d35568c7c667fc3d62b5e92d2f5d2411c006
yanghayong <hayong5621@daum.net>
2023-03-08 09:51:04 +0900

    my-note

---
+++ src/components/NoteList.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+export default function NoteList() {
+  return (
+    <ul className="list">
+      <li className="note">
+        <h4 className="title">안녕하세욧</h4>
+        <p>안녕하세요 1111</p>
+        <div className="bottom">
+          <div className="date">2022-10-20</div>
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      </li>
+      <li className="note">
+        <h4 className="title">안녕하세욧</h4>
+        <p>안녕하세요 1111</p>
+        <div className="bottom">
+          <div className="date">2022-10-20</div>
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      </li>
+      <li className="note">
+        <h4 className="title">안녕하세욧</h4>
+        <p>안녕하세요 1111</p>
+        <div className="bottom">
+          <div className="date">2022-10-20</div>
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      </li>
+      <li className="note">
+        <h4 className="title">안녕하세욧</h4>
+        <p>안녕하세요 1111</p>
+        <div className="bottom">
+          <div className="date">2022-10-20</div>
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      </li>
+      <li className="note">
+        <h4 className="title">안녕하세욧</h4>
+        <p>안녕하세요 1111</p>
+        <div className="bottom">
+          <div className="date">2022-10-20</div>
+          <button>수정</button>
+          <button>삭제</button>
+        </div>
+      </li>
+    </ul>
+  )
+}
