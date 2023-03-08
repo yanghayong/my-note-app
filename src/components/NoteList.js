c42da0bfdb08f866fc7c33bef05043a10dde2b76
yanghayong <hayong5621@daum.net>
2023-03-08 10:09:10 +0900

    note

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,53 +1,20 @@
 import React from 'react'
+import db from '../db/data.json'
 
 export default function NoteList() {
   return (
     <ul className="list">
-      <li className="note">
-        <h4 className="title">안녕하세욧</h4>
-        <p>안녕하세요 1111</p>
-        <div className="bottom">
-          <div className="date">2022-10-20</div>
-          <button>수정</button>
-          <button>삭제</button>
-        </div>
-      </li>
-      <li className="note">
-        <h4 className="title">안녕하세욧</h4>
-        <p>안녕하세요 1111</p>
-        <div className="bottom">
-          <div className="date">2022-10-20</div>
-          <button>수정</button>
-          <button>삭제</button>
-        </div>
-      </li>
-      <li className="note">
-        <h4 className="title">안녕하세욧</h4>
-        <p>안녕하세요 1111</p>
-        <div className="bottom">
-          <div className="date">2022-10-20</div>
-          <button>수정</button>
-          <button>삭제</button>
-        </div>
-      </li>
-      <li className="note">
-        <h4 className="title">안녕하세욧</h4>
-        <p>안녕하세요 1111</p>
-        <div className="bottom">
-          <div className="date">2022-10-20</div>
-          <button>수정</button>
-          <button>삭제</button>
-        </div>
-      </li>
-      <li className="note">
-        <h4 className="title">안녕하세욧</h4>
-        <p>안녕하세요 1111</p>
-        <div className="bottom">
-          <div className="date">2022-10-20</div>
-          <button>수정</button>
-          <button>삭제</button>
-        </div>
-      </li>
+      {db.notes.map((note) => (
+        <li className="note" key={note.id}>
+          <h4 className="title">{note.title}</h4>
+          <p>{note.detail}</p>
+          <div className="bottom">
+            <div className="date">{note.date}</div>
+            <button>수정</button>
+            <button>삭제</button>
+          </div>
+        </li>
+      ))}
     </ul>
   )
 }
