d9925f7607d2d4a9fe2999665537ca146acd7dfa
yanghayong <hayong5621@daum.net>
2023-03-09 11:11:22 +0900

    note

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,18 +1,18 @@
 // import db from '../db/data.json'
-import { useEffect, useState } from 'react'
-import Note from './Note'
+import { useEffect, useState } from "react"
+import Note from "./Note"
 
 export default function NoteList() {
   const [notes, setNotes] = useState([])
 
   useEffect(() => {
-    fetch('http://localhost:3001/notes?_sort=date&_order=desc')
+    fetch("http://localhost:3001/notes?_sort=date&_order=desc")
       .then(res => res.json())
       .then(data => setNotes(data))
   }, [])
 
   return (
-    <ul className='list'>
+    <ul className="list">
       {notes.map(note => (
         <Note note={note} key={note.id} />
       ))}
